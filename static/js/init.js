b9340eaa7de61b0d39175820b171a1c81acdf95d
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-06-22 00:49:16 +0800

    LOTS OF UPDATES :D

--- static/js/init.js
+++ static/js/init.js
@@ -1,35 +1,11 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-
-var instance = M.Carousel.init({
-  fullWidth: true,
-  indicators: true
-});
-
-// Or with jQuery
-
-$('.carousel.carousel-slider').carousel({
-  fullWidth: true,
-  indicators: true
-});
-      
 function readURL(input) {
   if (input.files && input.files[0]) {
-      var reader = new FileReader();
+    var reader = new FileReader();
 
-      reader.onload = function (e) {
-          $('#blah')
-              .attr('src', e.target.result)
-              .width(auto)
-              .height(auto);
-      };
+    reader.onload = function (e) {
+      $("#blah").attr("src", e.target.result).width(auto).height(auto);
+    };
 
-      reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(input.files[0]);
   }
 }
