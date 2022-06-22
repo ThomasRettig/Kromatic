ab2668a7e57939d2929145d5b3afd06fce9b2ab9
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-06-22 23:05:46 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -35,10 +35,9 @@ def not_found(e):
     return render_template("404.html"), 404
 
 # Handle 404 errors
-@app.errorhandler(404)
+@app.errorhandler(500)
 def server_error(e):
     return render_template("500.html"), 500
-
 
 if __name__ == "__main__":
     app.run()
