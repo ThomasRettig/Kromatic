f9b31ce9fd38e264f22b18c754664b159f2e3b29
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-06-23 12:02:09 +0800

    Perform URL validation

--- app/main.py
+++ app/main.py
@@ -1,32 +1,37 @@
 from flask import Flask, render_template, request
 import requests, time
+import os
+from urllib.parse import urlparse
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @app.route("/", methods=["POST"])
 def get_value():
     url = request.form["url"]
-    print(url)
-    r = requests.post(
-        "https://api.deepai.org/api/colorizer",
-        data={
-            "image": {url},
-        },
-        headers={"api-key": "7e7a76a4-3e3c-4176-8a42-a77a7fbf349c"},
-    )
-    print("reached")
+    path = urlparse(url).path
+    ext = os.path.splitext(path)[1]
+    # validate the URL
+    if ext != "png" or "jpg" or "jpeg":
+        print("Bad URL")
+        return render_template("500.html"), 500
+    else:
+        print(url)
+        r = requests.post(
+            "https://api.deepai.org/api/colorizer",
+            data={"image": {url}},
+            headers={"api-key": "7e7a76a4-3e3c-4176-8a42-a77a7fbf349c"},
+        )
+        print("Reached!")
 
-    time.sleep(10)
-    data = r.json()
-    print(data)
-    print(data["output_url"])
-    return render_template("io.html", org=url, data=data["output_url"])
+        time.sleep(10)
+        data = r.json()
+        print(data)
+        print(data["output_url"])
+        return render_template("io.html", org=url, data=data["output_url"])
 
 
 # Handle 404 errors
